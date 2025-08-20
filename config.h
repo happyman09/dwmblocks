e8de541567c1df8131d3817bca9f2363cc293026
happyman09 <125260988+happyman09@users.noreply.github.com>
2025-08-20 18:53:23 +0530

    Update config.h

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 Block blocks[] = {
-    {"", "/home/happyman9029/src/dwmblocks/moon", 60, 1},
-    {"", "/home/happyman9029/src/dwmblocks/ram", 2, 2},
-    {"", "/home/happyman9029/src/dwmblocks/netraf", 3, 3},
-    {"", "/home/happyman9029/src/dwmblocks/clock", 60, 4},
+    {"", "/path/to/script/moon", 60, 1},
+    {"", "/path/to/script/ram", 2, 2},
+    {"", "/path/to/script/netraf", 3, 3},
+    {"", "/path/to/script/clock", 60, 4},
 };
 char delim[] = " | ";
 unsigned int delimLen = 5;
